33c01d92aa8ad3f47e2c95147a62b4d69c77a535
root <root@ubu.qxv4ek31xfdulof3tsfaljckza.cx.internal.cloudapp.net>
2022-01-21 11:25:52 +0000

    first commit

---
+++ launchinstance.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0dd0ccab7e2801812"
+  instance_type     = "t2.micro"
+  key_name          = var.key_name
+  vpc_security_group_ids = [var.sg_id]
+  tags = {
+    Name = "Launched-via-Terraform"
+  }
+ 
+}
+
