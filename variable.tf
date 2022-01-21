9bc5c6b95aacd4ea6689fca22e5845ba49896068
root <root@ubu.qxv4ek31xfdulof3tsfaljckza.cx.internal.cloudapp.net>
2022-01-21 17:52:51 +0000

    adding variable TF

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "key_name" {
+
+  default = "rohit"
+
+}
+
+
+variable "pvt_key_name" {
+
+  default = "/root/.ssh/my-key.pem"
+
+}
+
+
+variable "sg_id" {
+
+  default = "sg-0557f753e5a2ea639"
+
+}
